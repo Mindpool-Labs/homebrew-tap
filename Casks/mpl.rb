26ef292d479d2dd643ed4a00087e043bac3f072b
Murali Raju <murali.raju@appliv.com>
2026-06-13 20:43:03 -0400

    cask(mpl): bump to 0.2.0 (engine adapters + lab-bench verbs)

--- Casks/mpl.rb
+++ Casks/mpl.rb
@@ -1,6 +1,6 @@
 cask "mpl" do
-  version "0.1.1"
-  sha256 "ee633262ec35466fe0bb2328eceeeacf4212ec670c77d5a2e04809155eb32a4a"
+  version "0.2.0"
+  sha256 "c98b38859c8c1ddee778daa8064a1980ff542e7659c5e16fcb6e47d317a5bea1"
 
   url "https://github.com/Mindpool-Labs/mpl/releases/download/v#{version}/mpl-aarch64-apple-darwin.tar.gz"
   name "mpl"
