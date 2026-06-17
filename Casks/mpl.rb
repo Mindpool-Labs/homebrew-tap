5d890cb2c6a0101f3639485d55b6169559200e29
Murali Raju <murali.raju@appliv.com>
2026-06-17 13:19:25 +0000

    cask(mpl): bump to 0.11.0 (automated by release.yml)

--- Casks/mpl.rb
+++ Casks/mpl.rb
@@ -1,6 +1,6 @@
 cask "mpl" do
-  version "0.10.0"
-  sha256 "c6bf66d3abc7ea86b6c475f5eb9ce46d0a67fd921d96ef533b6db758bbb56f09"
+  version "0.11.0"
+  sha256 "3030773b6c0b0d1829b442b7dbe97bdd3e776ec9bcaa0abbcb082420e923509c"
 
   url "https://github.com/Mindpool-Labs/mpl/releases/download/v#{version}/mpl-aarch64-apple-darwin.tar.gz"
   name "mpl"
