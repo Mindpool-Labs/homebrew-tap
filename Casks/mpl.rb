6b664be191f1de6bd620e2879bb09f3287cf2bf4
Mindpool Labs <oss@mindpool.io>
2026-07-12 22:48:23 +0000

    cask(mpl): bump to 0.31.0 (automated by release.yml)

--- Casks/mpl.rb
+++ Casks/mpl.rb
@@ -1,6 +1,6 @@
 cask "mpl" do
-  version "0.30.0"
-  sha256 "a2c8f30d09dd43a0b0feaf20d9b57605f560ce285becb7b467d3a2815da27b6d"
+  version "0.31.0"
+  sha256 "dc038dd4aa06926580fa553ec90b67b7af6b209bea230c7eb0d19c1b27f522b6"
 
   url "https://github.com/Mindpool-Labs/mpl/releases/download/v#{version}/mpl-aarch64-apple-darwin.tar.gz"
   name "mpl"
